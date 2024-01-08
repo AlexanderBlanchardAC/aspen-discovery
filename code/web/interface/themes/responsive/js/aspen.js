07fb7336b5c832a327860fb8d63514ac28d2ef5b
Alexander Blanchard <alexander.blanchard@ptfs-europe.com>
2024-01-08 08:27:17 +0000

    Resolved error 500

--- code/web/interface/themes/responsive/js/aspen.js
+++ code/web/interface/themes/responsive/js/aspen.js
@@ -14347,6 +14347,27 @@ AspenDiscovery.Searches = (function(){
 		}
 	}
 }(AspenDiscovery.Searches || {}));
+AspenDiscovery.Summon = (function(){
+	return {
+		getSummonResults: function(searchTerm){
+			var url = Globals.path + "/Search/AJAX";
+			var params = "method=getSummonResults&searchTerm=" + encodeURIComponent(searchTerm);
+			var fullUrl = url + "?" + params;
+			$.ajax({
+				url: fullUrl,
+				dataType:"json",
+				success: function(data) {
+					var searchResults = data.formattedResults;
+					if (searchResults) {
+						if (searchResults.length > 0){
+							$("#summonSearchResultsPlaceholder").html(searchResults);
+						}
+					}
+				}
+			});
+		}
+	}
+}(AspenDiscovery.Summon || {}));
 AspenDiscovery.SideLoads = (function(){
 	return {
 		deleteMarc: function (sideLoadId, fileName, fileIndex) {
@@ -15434,24 +15455,3 @@ AspenDiscovery.PalaceProject = (function () {
 		},
 	}
 }(AspenDiscovery.PalaceProject || {}));
-AspenDiscovery.Summon = (function(){
-	return {
-		getSummonResults: function(searchTerm){
-			var url = Globals.path + "/Search/AJAX";
-			var params = "method=getSummonResults&searchTerm=" + encodeURIComponent(searchTerm);
-			var fullUrl = url + "?" + params;
-			$.ajax({
-				url: fullUrl,
-				dataType:"json",
-				success: function(data) {
-					var searchResults = data.formattedResults;
-					if (searchResults) {
-						if (searchResults.length > 0){
-							$("#summonSearchResultsPlaceholder").html(searchResults);
-						}
-					}
-				}
-			});
-		}
-	}
-}(AspenDiscovery.Summon || {}));
