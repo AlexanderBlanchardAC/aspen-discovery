9a0d256062ec0501a941a34ec19c9571bfdb1510
Alexander Blanchard <alexander.blanchard@ptfs-europe.com>
2024-06-18 11:20:40 +0100

    refactor: remove unused code
    Remove unused code for seperate grapes web builder module

--- code/web/sys/DBMaintenance/grapes_web_builder_updates.php
+++ code/web/sys/DBMaintenance/grapes_web_builder_updates.php
@@ -1,21 +1,6 @@
 <?php /** @noinspection SqlResolve */
 function getGrapesWebBuilderUpdates() {
 	return [
-		'grapes_web_builder_module' => [
-			'title' => 'Grapes Web Builder Module',
-			'description' => 'Create Grapes Web Builder Module',
-			'sql' => [
-				"INSERT INTO modules (name, indexName, backgroundProcess) VALUES ('Grapes Web Builder', 'grapes_web_builder', '')",
-			],
-		],
-
-		'grapes_web_builder_module_monitoring_and_indexing' => [
-			'title' => 'Grapes Web Builder Module - Monitoring, indexing',
-			'description' => 'Update Grapes Web Builder module to monitor logs and start indexer',
-			'sql' => [
-				"UPDATE modules set backgroundProcess='web_indexer', logClassPath='/sys/WebsiteIndexing/WebsiteIndexLogEntry.php', logClassName='WebsiteIndexLogEntry' WHERE name = 'Grapes Web Builder'",
-			],
-		],
 		'grapes_web_builder' => [
 			'title' => 'Web Builder Basic Pages',
 			'description' => 'Setup Basic Pages within Web Builder',
@@ -26,13 +11,6 @@ function getGrapesWebBuilderUpdates() {
 					urlAlias VARCHAR(100),
 					teaser VARCHAR(512)
 				) ENGINE=INNODB",
-			],
-		],
-		'remove_grapes_js_module' => [
-			'title' => 'Remove Seperate Grapes JS Module',
-			'description' => 'Remove seperate Grapes JS Module form modules table',
-			'sql' => [
-				"ALTER TABLE modules DROP COLUMN `Grapes Web Builder`;"
 			],
 		],
 		'add_template_options' => [
