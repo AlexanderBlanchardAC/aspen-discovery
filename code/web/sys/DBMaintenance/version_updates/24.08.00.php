3eb671b72630071fce0d354b90d0ebf0c893fe7b
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2024-07-15 13:31:56 +0100

    feat: add showInSearchFacet col into location table

--- code/web/sys/DBMaintenance/version_updates/24.08.00.php
+++ code/web/sys/DBMaintenance/version_updates/24.08.00.php
@@ -24,6 +24,15 @@ function getUpdates24_08_00(): array {
 
 		//pedro - PTFS-Europe
 
+		//chloe - PTFS-Europe
+		'show_in_search_facet_column' => [
+			'title' => 'Show In Search Facet Column',
+			'description' => 'Adds the showInSearchFacet column to the Location table',
+			// 'continueOnError' => false,
+			'sql' => [
+				'ALTER TABLE location ADD COLUMN showInSearchFacet TINYINT(1) DEFAULT 1'
+			]
+			],
 		//other
 
 	];
