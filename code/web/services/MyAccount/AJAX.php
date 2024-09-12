f89d6e8358a47c7a638c96b1bf391189b0978c59
Alexander Blanchard <alexander.blanchard@ptfs-europe.com>
2024-09-12 17:27:04 +0100

    feat: Add Num Campaigns to Account Home Page
    Add number of campaigns user is enrolled in to account home page.

--- code/web/services/MyAccount/AJAX.php
+++ code/web/services/MyAccount/AJAX.php
@@ -8936,6 +8936,15 @@ class MyAccount_AJAX extends JSON_Action {
 		}
 	}
 
+	public function getEnrolledCampaigns() {
+		$userId = UserAccount::getActiveUserId();
+		$enrolledCampaigns = Campaign::getUserEnrolledCampaigns($userId);
+		return [
+			'success' => true,
+			'numCampaigns' => count($enrolledCampaigns)
+		];
+	}
+
 	function getYearInReviewSlide() : array {
 		$result = [
 			'success' => false,
