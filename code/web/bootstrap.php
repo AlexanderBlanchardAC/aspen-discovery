3c1e3c020e078c2cb1c3a32a714565f2df037eea
Alexander <alexanderblanchardac@yahoo.com>
2025-02-12 15:22:21 +0000

    fix: Fix Hooks not running

--- code/web/bootstrap.php
+++ code/web/bootstrap.php
@@ -219,7 +219,7 @@ try {
 	//Modules are not installed yet
 }
 
-if (array_key_exists('Community', $enabledModules)) {
+if (array_key_exists('Community Engagement', $enabledModules)) {
 	require_once ROOT_DIR . '/sys/CommunityEngagement/action-hooks.php';
 }
 
