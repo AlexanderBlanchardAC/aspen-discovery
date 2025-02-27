78b1c28b28737156241da473c4f83571f5433fb2
Alexander <alexanderblanchardac@yahoo.com>
2025-02-27 16:46:00 +0000

    DIS-352: refactor: Opt in to campaign emails

--- code/web/services/MyAccount/AJAX.php
+++ code/web/services/MyAccount/AJAX.php
@@ -9261,21 +9261,6 @@ class MyAccount_AJAX extends JSON_Action {
 			];
 		}
 
-		// $userId = UserAccount::getActiveUserId();
-		// if (!$userId) {
-		// 	return [
-		// 		'success' => false,
-		// 		'title' => translate([
-		// 			'text' => 'Error',
-		// 			'isPublicFacing' => true
-		// 		]),
-		// 		'message' => translate([
-		// 			'text' => 'User is not logged in.',
-		// 			'isPublicFacing' => true
-		// 		])
-		// 	];
-		// }
-
 		$userCampaign = new UserCampaign();
 		$userCampaign->userId = $userId;
 		$userCampaign->campaignId = $campaignId;
@@ -9350,6 +9335,9 @@ class MyAccount_AJAX extends JSON_Action {
 			$campaign->update();
 			return [
 				'success' => true,
+				'showEmailOptInPromptFlag' => true,
+				'campaignId' => $campaignId,
+				'userId' => $userId,
 				'title' => translate([
 					'text' => 'Success',
 					'isPublicFacing' => true
@@ -9370,9 +9358,6 @@ class MyAccount_AJAX extends JSON_Action {
 					'text' => 'Failed to enroll user in campaign.',
 					'isPublicFacing' => true
 				]),
-				'showEmailOptInPrompt' => true,
-				'campaignId' => $campaignId,
-				'userId' => $userId
 			];
 		}
 	}
