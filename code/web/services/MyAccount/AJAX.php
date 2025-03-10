17ae737b4c3cbe9c5f120907803a2cc5fa3975d3
Alexander <alexanderblanchardac@yahoo.com>
2025-03-10 23:43:29 +0000

    DIS-352: refactor: Reorganise campaign enroll and email
    -Reorganise campaign enrollment process so emails are only sent if the
    user opts in.
    -Add send email on enrollment and on milestone completion
    -Add send email on campaign ending date
    
    cron

--- code/web/services/MyAccount/AJAX.php
+++ code/web/services/MyAccount/AJAX.php
@@ -9243,9 +9243,11 @@ class MyAccount_AJAX extends JSON_Action {
 	public function enrollCampaign() {
 		require_once ROOT_DIR . '/sys/CommunityEngagement/UserCampaign.php';
 		require_once ROOT_DIR . '/sys/CommunityEngagement/Campaign.php';
+		require_once ROOT_DIR . '/sys/Account/User.php';
 
 		$campaignId = $_GET['campaignId'] ?? null;
 		$userId = $_GET['userId'] ?? null;
+
 
 		if (!$campaignId || !$userId) {
 			return[
@@ -9333,9 +9335,11 @@ class MyAccount_AJAX extends JSON_Action {
 			$campaign->enrollmentCounter++;
 			$campaign->currentEnrollments++;
 			$campaign->update();
+
+
+
 			return [
 				'success' => true,
-				'showEmailOptInPromptFlag' => true,
 				'campaignId' => $campaignId,
 				'userId' => $userId,
 				'title' => translate([
@@ -9588,7 +9592,6 @@ class MyAccount_AJAX extends JSON_Action {
 			}
 		}
 	}
-	
 
 	function getYearInReviewSlide() : array {
 		$result = [
