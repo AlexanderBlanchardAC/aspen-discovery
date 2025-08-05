38e99880684ec8ad4e79b07159195045e0895c09
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-08-05 13:13:36 +0100

    temp

--- code/web/services/API/UserAPI.php
+++ code/web/services/API/UserAPI.php
@@ -6918,8 +6918,8 @@ class UserAPI extends AbstractAPI {
 		$imageSettings = Campaign::getImageDisplaySettings($user, $library);
 		global $logger;
 		$logger->log("IMAGE SETTINGS: " . print_r($imageSettings, true), Logger::LOG_ERROR);
-		$logger->log("USER: " . print_r($user, true), Logger::LOG_ERROR);
-		$logger->log("USER: " . print_r($library, true), Logger::LOG_ERROR);
+		// $logger->log("USER: " . print_r($user, true), Logger::LOG_ERROR);
+		// $logger->log("USER: " . print_r($library, true), Logger::LOG_ERROR);
 
 
 
