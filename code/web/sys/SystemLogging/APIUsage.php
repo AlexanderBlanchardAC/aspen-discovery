d1021a538cf00189138d0e3d807f93db6c47023c
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-11-06 11:12:21 +0000

    logging

--- code/web/sys/SystemLogging/APIUsage.php
+++ code/web/sys/SystemLogging/APIUsage.php
@@ -24,6 +24,8 @@ class APIUsage extends DataObject {
 	}
 
 	static function incrementStat($module, $method, /*$libraryId = null*/) : void {
+		global $logger; 
+		$logger->log("incrementStat called - Module:  and method: " . $module, $method, Logger::LOG_ERROR);
 		try {
 			$apiUsage = new APIUsage();
 			$apiUsage->year = date('Y');
@@ -33,15 +35,28 @@ class APIUsage extends DataObject {
 			$apiUsage->module = $module;
 			$apiUsage->method = $method;
 			// $apiUsage->libraryId = $libraryId;
+			$logger->log("Looking for existing record...", Logger::LOG_ERROR);
+
 			if ($apiUsage->find(true)) {
 				$apiUsage->numCalls++;
+				$logger->log("Found existing record, updating. New count: " . $apiUsage->numCalls, Logger::LOG_ERROR);
+
 				$apiUsage->update();
 			} else {
 				$apiUsage->numCalls = 1;
+				$logger->log("No existing record, inserting new record", Logger::LOG_ERROR);
+
 				$apiUsage->insert();
 			}
+			        $logger->log("incrementStat completed successfully", Logger::LOG_ERROR);
+
 		} catch (PDOException) {
 			//This happens if the table has not been created, ignore it
+			        $logger->log("PDOException in incrementStat: " . $e->getMessage(), Logger::LOG_ERROR);
+
+		} catch (Exception $e) {
+			        $logger->log("Exception in incrementStat: " . $e->getMessage(), Logger::LOG_ERROR);
+
 		}
 	}
 
