6095ec64af0bd214a1cea9cf2a4574d8fb2dbbd1
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-11-12 13:51:15 +0000

    DIS-1578: feat: add location to calendar view
    Add the location of the event to the calendar

--- code/web/services/Events/Calendar.php
+++ code/web/services/Events/Calendar.php
@@ -280,6 +280,7 @@ class Events_Calendar extends Action {
 							'link' => $url,
 							'formattedTime' => $formattedTime,
 							'isCancelled' => $isCancelled,
+							'location' => $result['branch'][0],
 						];
 					}
 				}
